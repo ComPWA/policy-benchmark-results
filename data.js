c401414609c3fd2f8e793c2d29ee81a5e9549639
github-action-benchmark <github@users.noreply.github.com>
2026-07-12 13:56:58 +0000

    add ComPWA policy benchmark results (pytest) benchmark result for 0391e316b53666a09f7e62cfcfed15593f6941d8

---
+++ data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1783864618528,
+  "repoUrl": "https://github.com/ComPWA/policy",
+  "entries": {
+    "ComPWA policy benchmark results": [
+      {
+        "commit": {
+          "author": {
+            "email": "29308176+redeboer@users.noreply.github.com",
+            "name": "Remco de Boer",
+            "username": "redeboer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0391e316b53666a09f7e62cfcfed15593f6941d8",
+          "message": "DX: implement benchmark for `check-dev-files` (#648)",
+          "timestamp": "2026-07-12T15:56:16+02:00",
+          "tree_id": "b7a01faa360c1c28228ea6be964e2d7a61cb5fb0",
+          "url": "https://github.com/ComPWA/policy/commit/0391e316b53666a09f7e62cfcfed15593f6941d8"
+        },
+        "date": 1783864618308,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "benchmarks/test_check_dev_files.py::test_check_dev_files",
+            "value": 0.42131729270677626,
+            "unit": "iter/sec",
+            "range": "stddev: 0.01727454353703041",
+            "extra": "mean: 2.373508083599998 sec\nrounds: 5"
+          }
+        ]
+      }
+    ]
+  }
+}
